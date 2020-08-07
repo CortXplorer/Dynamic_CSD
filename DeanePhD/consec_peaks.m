187559cf6e2aaa9804a49b041f964ed69d14227b
kdeane <katrina.deane@lin-magdeburg.de>
2020-08-07 13:33:00 +0200

    included BL input to fnc for latency detection time after the first stim

--- DeanePhD/consec_peaks.m
+++ DeanePhD/consec_peaks.m
@@ -1,7 +1,10 @@
-function [peakout,latencyout] = consec_peaks(spectin, num_stim, dur_stim, start_time)
+function [peakout,latencyout] = consec_peaks(spectin, num_stim, dur_stim, start_time, BL)
 
 if ~exist('start_time','var')
-    start_time = 0; % 3 ms to avoid that the sink starts directly at 0 ms
+    start_time = 1; % 1 ms to avoid that the sink starts directly at 0 ms
+end
+if ~exist('BL','var')
+    BL  = 200; % 200 ms before the first stim onset
 end
 if ~exist('num_stim','var')
     num_stim  = 2;
@@ -11,23 +14,22 @@ if ~exist('dur_stim','var')
 end
 
 % structin   = Data(imeas).(para{ipar})(istim).(layer{ilay});
-% sinkonset_struct = Data(imeas).Sinkonset(istim).(layer{ilay});
 
 %preallocation of onset detection window containers
 peakout    = nan(1,num_stim);
 latencyout = nan(1,num_stim);
 det_on     = nan(1,num_stim);
 det_off    = nan(1,num_stim);
-det_jump   = (dur_stim-start_time)/num_stim;
+det_jump   = dur_stim/num_stim;
 
 % fill detection window containers
 for idet = 1:num_stim
     if idet == 1
-        det_on(idet) = start_time;
+        det_on(idet) = start_time+BL;
     else
         det_on(idet) = det_on(idet-1) + det_jump;
     end
-    det_off(idet) = det_on(idet) + det_jump-1;
+    det_off(idet) = det_on(idet) + det_jump-2;
 end
 
 
@@ -43,6 +45,6 @@ for iSti = 1:num_stim
     % find peak power and peak latency
     peakout(iSti) =  nanmax(nanmax(det_win));
     [~,latencyout(iSti)] = find(det_win == peakout(iSti));
-    latencyout(iSti) = latencyout(iSti) + det_on(iSti);
+    latencyout(iSti) = latencyout(iSti) + det_on(iSti) - BL;
      
 end
