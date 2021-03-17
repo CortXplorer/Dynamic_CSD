3226e1db3854c4629453cfa0eef2356db10bf9e7
kdeane <katrina.deane@lin-magdeburg.de>
2021-03-17 12:40:24 +0100

    short script for an auditory demonstration of AM

---
+++ DeanePhD/AMwav.m
@@ -0,0 +1,14 @@
+%% amp mod
+Fs = 22400;
+t=0:1/Fs:0.96;
+
+frqHz = 400;
+ampcarrier = 1;
+messagesignal = sin(2*pi*5*t);
+signofcarrier = sin(2*pi*frqHz*t);
+
+x=2*(ampcarrier+messagesignal).*signofcarrier;
+% plot(x)
+sound(x,Fs);
+
+audiowrite('AmpMod.wav',x,Fs)
